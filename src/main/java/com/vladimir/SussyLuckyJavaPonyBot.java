a0788a35a8443fd671dae2adbff5e211827fb2bb
Vladimir <gagnguy@mail.ru>
2026-02-04 15:17:13 +0300

    убрал неиспользуемый иморт

--- src/main/java/com/vladimir/SussyLuckyJavaPonyBot.java
+++ src/main/java/com/vladimir/SussyLuckyJavaPonyBot.java
@@ -10,7 +10,7 @@ import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
+
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
